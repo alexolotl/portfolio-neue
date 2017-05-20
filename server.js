e3233a924dbd6d624885eb6496f78e8b3e5c04c7
Alex Zisis <azisis219@gmail.com>
2017-05-20 16:46:03 -0400

    commit

--- server.js
+++ server.js
@@ -9,6 +9,5 @@ app.get('/', function(req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  // var port = server.address().port;
-  // console.log("App now running on port", port);
+  
 });
