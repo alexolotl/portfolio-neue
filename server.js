46ece9cf406fd333e56568cea95d4063658c8b91
Alex Zisis <azisis219@gmail.com>
2017-05-20 16:25:37 -0400

    hi

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var express = require('express')
+var express = require('express');
 
-var app = express()
+var app = express();
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
 });
